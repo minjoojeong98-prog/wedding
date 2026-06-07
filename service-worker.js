9a9e05c8fa42e3ebbe29aaf608668c962b3452d6
minjoojeong98-prog <minjoojeong98@gmail.com>
2026-06-07 13:34:59 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,17 @@
 // CACHE VERSION - 버전 올리면 PWA 캐시 강제 갱신
-const CACHE_NAME = 'wedding-planner-v3';
-const urlsToCache = [
-  '/wedding/',
-  '/wedding/index.html',
-  '/wedding/manifest.json',
-];
-
-// 설치: 새 캐시 생성
+const CACHE_NAME = 'wedding-planner-v7';
+const urlsToCache = ['/wedding/','/wedding/index.html','/wedding/manifest.json'];
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
-// 활성화: 이전 버전 캐시 삭제
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-// 요청: 네트워크 우선, 실패 시 캐시
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const clone = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
